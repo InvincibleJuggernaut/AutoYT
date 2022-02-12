dd2e7ff69a1e2b6027de6220908f766d6e8af8b4
Manikandan Nair <mani4pmn@gmail.com>
2022-02-12 08:08:25 -0500

    Minor change to user input method
    Takes user inputs in the same line

--- YT_prompt.sh
+++ YT_prompt.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 export PATH="$HOME/.local/bin:$PATH"
 
-echo Enter URL
-read URL
-
-echo Enter Category
-read CATEGORY
+read -p "Enter URL: " URL
+read -p "Enter Category: " CATEGORY
 
 yt-dlp $URL --format mp4
 
 mv *.mp4 /home/$USER/$CATEGORY/
+
+echo "COMPLETED SUCCESSFULLY"
+
+sleep 10
